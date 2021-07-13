7201329f357e2908d248e90827997ecf0893484d
Lia Nemeth <lia.nemeth@nytimes.com>
2021-07-13 10:58:18 -0400

    summation of primes in go

---
+++ go/summation_of_primes.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func SieveOfEratosthenes(n int) []int {
+	// create a boolean array, and initialize all as true
+	integers := make([]bool, n+1)
+	for i := 2; i < n+1; i++ {
+		integers[i] = true
+	}
+
+	for p := 2; p*p <= n; p++ {
+		if integers[p] == true {
+			for i := p * 2; i <= n; i = p + i {
+				integers[i] = false
+			}
+		}
+	}
+	var primes []int
+	for p := 2; p <= n; p++ {
+		if integers[p] == true {
+			primes = append(primes, p)
+		}
+	}
+	return primes
+}
+
+func main() {
+	pr := SieveOfEratosthenes(2000000)
+	sum := 0
+	for _, p := range pr {
+		sum += p
+	}
+	fmt.Println(sum)
+}
